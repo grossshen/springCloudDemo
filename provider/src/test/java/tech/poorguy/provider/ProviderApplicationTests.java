c81c93da6d4ec29106f5aa6cdddff3449a8356be
poorguy <494939649@qq.com>
2019-02-21 08:04:18 +0800

    multy register center

---
+++ provider/src/test/java/tech/poorguy/provider/ProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package tech.poorguy.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
